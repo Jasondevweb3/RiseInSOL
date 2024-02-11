03b0b88870de8ab55b1dfa937e7d8710b0a100a3
Jasondevweb3 <jasonweb3.dev@gmail.com>
2024-02-11 15:48:37 +0100

    gh

--- restaurant/src/components/Form.tsx
+++ restaurant/src/components/Form.tsx
@@ -80,19 +80,7 @@ const ReviewForm: FC<FormProps> = ({
                     />
                 </div>
 
-                <div className="mb-4">
-                    <label className="block text-gray-400 text-sm font-bold mb-2">
-                        location
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="location"
-                        type="text"
-                        placeholder="location"
-                        value={location}
-                        onChange={(e) => setlocation(e.target.value)}
-                    />
-                </div>
+
 
                 <div className="flex items-center justify-between">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
